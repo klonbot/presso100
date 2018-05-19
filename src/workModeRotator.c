6d4795e6b42af1749f2d2993c64387ad098f6219
Рыбаков Дмитрий Николаевич <rybakov_dn@tomsk.lemz.ru>
2018-05-19 18:12:18 +0700

    Добавил авто изменение направления вращения

--- src/workModeRotator.c
+++ src/workModeRotator.c
@@ -23,13 +23,47 @@ typedef enum
 
 //------------------------------------------------------------------------------
 
-enum {timePhase_s = 7};
+enum {timePhase_s = 4};
+
+typedef enum {directionRotation_right, directionRotation_left} directionRotation_t;
 
 //------------------------------------------------------------------------------
 
 phaseRotation_t m_phaseRotation = phaseRotation_1;
 static u32 m_cycles_cnt = 0;
-u32 m_round_cnt = 0;
+static u32 m_round_cnt = 0;
+static directionRotation_t m_directionRotation = directionRotation_right;
+
+//------------------------------------------------------------------------------
+/**
+ * Переключает направление вращения
+ */
+void switchRotation(void)
+{
+    if (directionRotation_right == m_directionRotation)
+    {
+        m_directionRotation = directionRotation_left;
+    }
+	else
+    if (directionRotation_left == m_directionRotation)
+    {
+        m_directionRotation = directionRotation_right;
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+ * Переходит на следующий круг
+ */
+void nextRound(void)
+{
+    ++m_round_cnt;
+    if (m_round_cnt > 2)
+    {
+        m_round_cnt = 0;
+        switchRotation();
+    }
+}
 
 //------------------------------------------------------------------------------
 /**
@@ -41,7 +75,7 @@ void nextPhase (void)
     if (m_phaseRotation >= phaseRotation_num)
     {
         m_phaseRotation = phaseRotation_1;
-        ++m_round_cnt;
+        nextRound();
     }
 }
 
@@ -65,32 +99,65 @@ void nextPhaseHandler(void)
  */
 void phaseSwitch(void)
 {
-    switch(m_phaseRotation)
+    if (directionRotation_right == m_directionRotation)
     {
-        case phaseRotation_1:
-            setPneumoChannelPumpOn(pneumoCnl_1);    // надувается
-            setPneumoChannelHold(pneumoCnl_4);      // удерживается
-            setPneumoChannelPumpOut(pneumoCnl_3);   // спускается
-            setPneumoChannelPumpOut(pneumoCnl_2);   // спускается - уже спущено
-            break;
-        case phaseRotation_2:
-            setPneumoChannelPumpOn(pneumoCnl_2);    // надувается
-            setPneumoChannelHold(pneumoCnl_1);      // удерживается
-            setPneumoChannelPumpOut(pneumoCnl_4);   // спускается
-            setPneumoChannelPumpOut(pneumoCnl_3);   // спускается - уже спущено
-            break;
-        case phaseRotation_3:
-            setPneumoChannelPumpOn(pneumoCnl_3);    // надувается
-            setPneumoChannelHold(pneumoCnl_2);      // удерживается
-            setPneumoChannelPumpOut(pneumoCnl_1);   // спускается
-            setPneumoChannelPumpOut(pneumoCnl_4);   // спускается - уже спущено
-            break;
-        case phaseRotation_4:
-            setPneumoChannelPumpOn(pneumoCnl_4);    // надувается
-            setPneumoChannelHold(pneumoCnl_3);      // удерживается
-            setPneumoChannelPumpOut(pneumoCnl_2);   // спускается
-            setPneumoChannelPumpOut(pneumoCnl_1);   // спускается - уже спущено
-            break;
+        switch(m_phaseRotation)
+        {
+            case phaseRotation_1:
+                setPneumoChannelPumpOn(pneumoCnl_1);    // надувается
+                setPneumoChannelHold(pneumoCnl_4);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_3);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_2);   // спускается - уже спущено
+                break;
+            case phaseRotation_2:
+                setPneumoChannelPumpOn(pneumoCnl_2);    // надувается
+                setPneumoChannelHold(pneumoCnl_1);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_4);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_3);   // спускается - уже спущено
+                break;
+            case phaseRotation_3:
+                setPneumoChannelPumpOn(pneumoCnl_3);    // надувается
+                setPneumoChannelHold(pneumoCnl_2);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_1);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_4);   // спускается - уже спущено
+                break;
+            case phaseRotation_4:
+                setPneumoChannelPumpOn(pneumoCnl_4);    // надувается
+                setPneumoChannelHold(pneumoCnl_3);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_2);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_1);   // спускается - уже спущено
+                break;
+        }
+    }
+    if (directionRotation_left == m_directionRotation)
+    {
+        switch(m_phaseRotation)
+        {
+            case phaseRotation_1:
+                setPneumoChannelPumpOn(pneumoCnl_1);    // надувается
+                setPneumoChannelHold(pneumoCnl_2);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_3);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_4);   // спускается - уже спущено
+                break;
+            case phaseRotation_2:
+                setPneumoChannelPumpOn(pneumoCnl_4);    // надувается
+                setPneumoChannelHold(pneumoCnl_1);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_2);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_3);   // спускается - уже спущено
+                break;
+            case phaseRotation_3:
+                setPneumoChannelPumpOn(pneumoCnl_3);    // надувается
+                setPneumoChannelHold(pneumoCnl_4);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_1);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_2);   // спускается - уже спущено
+                break;
+            case phaseRotation_4:
+                setPneumoChannelPumpOn(pneumoCnl_2);    // надувается
+                setPneumoChannelHold(pneumoCnl_3);      // удерживается
+                setPneumoChannelPumpOut(pneumoCnl_4);   // спускается
+                setPneumoChannelPumpOut(pneumoCnl_1);   // спускается - уже спущено
+                break;
+        }
     }
 }
 
